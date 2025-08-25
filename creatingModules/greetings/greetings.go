bd359e29c908541f042ebb568b8f56cfb6b680f7
Parth Kalani <parthkalani1899@gmail.com>
2025-08-25 12:49:27 +0530

    returning and handling errors

--- creatingModules/greetings/greetings.go
+++ creatingModules/greetings/greetings.go
@@ -1,8 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	// error handling
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil // nil - no error
 }
--- creatingModules/hello/hello.go
+++ creatingModules/hello/hello.go
@@ -3,9 +3,24 @@ package main
 import (
     "fmt"
     "example.com/greetings"
+	"log"
 )
 
 func main() {
-    message := greetings.Hello("Parth")
+
+	// Set properties of the predefined Logger
+    log.SetPrefix("greetings: ") // log entry prefix
+    // log.SetFlags(0) // flag to disable printing the time, source file, and line number.
+
+    message, err := greetings.Hello("Parth")
+	
+	// handle error
+	if(err != nil) {
+		log.Fatal(err)
+	}
+
     fmt.Println(message)
 }
+
+// greetings: 2025/08/25 12:45:06 empty name
+// exit status 1
