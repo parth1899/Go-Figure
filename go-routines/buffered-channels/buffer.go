32d9fca37826fdb44bb44877c526a6a0ad4f2154
Parth Kalani <parthkalani1899@gmail.com>
2025-09-01 01:00:11 +0530

    buffered channels basics

---
+++ go-routines/buffered-channels/buffer.go
@@ -0,0 +1,22 @@
+package main
+
+// Buffered channels
+
+// channel buffering is FIFO, channels in Go are FIFO (first-in, first-out) queues
+
+import "fmt"
+
+func main() {
+	c := make(chan int, 3) // second argument is the buffer size
+	// Sends to a buffered channel blocks only when the buffer is full.
+	// Receives blocks when the buffer is empty.
+
+	c <- 1
+	c <- 2
+	c <- 3
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+}
